c5dccc688c1cc8d36611f21f0460b119beaeca30
Handze1 <ahandzel7418@sdsu.edu>
2023-05-18 13:45:59 -0700

    Update3

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import json
+from urllib.request import urlopen
 import pandas as pd
 import streamlit as st
 import plotly.express as px
-from urllib.request import urlopen
+
 
 
 with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
@@ -15,15 +16,17 @@ def read_csv_pop(file):
     Cleaning up population Data
     Converting County Fips from int to string, then using zfill to make sure fips have 5 numbers
     """
-    df_pop = pd.read_csv("covid_county_population_usafacts.csv")
+    df_pop = pd.read_csv(file)
     df_pop['countyFIPS'] = df_pop.countyFIPS.astype(str).str.zfill(5)
     df_pop = df_pop.iloc[:, [0, 3]].set_index('countyFIPS')
     df_pop = df_pop.drop(['00000'], axis=0)
     return df_pop
 
 
-# Reading in CSV files for confirmed cases and deaths
 def read_csv_cases(file):
+    """
+    Reading in CSV files for confirmed cases and covid deaths
+    """
     df_read = pd.read_csv(file)
     df_read['countyFIPS'] = df_read.countyFIPS.astype(str).str.zfill(5)
     df_read = df_read.drop(['County Name', 'State', 'StateFIPS'], axis=1).set_index('countyFIPS')
@@ -31,10 +34,12 @@ def read_csv_cases(file):
     return df_read
 
 
-# Function to check for full weeks only
-# Transposed dataframe then used resample to check for incomplete weeks
-# Incomplete weeks were added to drop list
 def drop_dates(df):
+    """
+    Function to check for full weeks only
+    Transposed dataframe then used resample to check for incomplete weeks
+    Incomplete weeks were added to drop list
+    """
     drop_list = []
     for i in df.T.resample('W-Sat'):
         if len(i[1]) != 7:
@@ -42,11 +47,13 @@ def drop_dates(df):
     return drop_list
 
 
-# Convert headers to datetime
-# Using resample to convert days to full weeks and summing values
-# Dropping weeks that are not full using the drop list
-# Creating new column for the sum of covid cases/deaths per week
 def lineplot_clean_data(df):
+    """
+    Convert headers to datetime
+    Using resample to convert days to full weeks and summing values
+    Dropping weeks that are not full using the drop list
+    Creating new column for the sum of covid cases/deaths per week
+    """
     df = df.reset_index().drop(['countyFIPS'], axis=1)
     df.columns = pd.to_datetime(df.columns)
     df = df.resample('W-Sat', axis=1).sum().drop(dates_to_drop, axis=1).T
@@ -54,34 +61,42 @@ def lineplot_clean_data(df):
     clean_data_for_vis = df.iloc[:, -1]
     return clean_data_for_vis
 
-# Dropping fips that are unallocated
-# Converting headers to datetime
-# Using resample to convert days to full weeks (Sun-Sat) anc calculated the mean
+
 def choropleth_clean_data(df):
+    """
+    Dropping fips that are unallocated
+    Converting headers to datetime
+    Using resample to convert days to full weeks (Sun-Sat) anc calculated the mean
+    Using drop list to drop incomplete weeks
+    Using div() function to divide the mean of each week by the population size to get per 100,000
+    Used melt to convert convert dataframe into two columns (variable and value)
+
+    """
     df = df.reset_index()
     df = df.set_index('countyFIPS')
     df = df.drop(['00000'], axis=0)
     df.columns = pd.to_datetime(df.columns)
     df = df.resample('W-Sat', axis=1).mean()
-    # Using drop list to drop incomplete weeks
     df = df.drop(dates_to_drop, axis=1)
-    # Using div() function to divide the mean of each week by the population size to get per 100,000
     df = df.div(population.iloc[:, -1], axis='rows').multiply(100000).reset_index()
     df.columns = df.columns.astype(str)
-    # Used melt to convert convert dataframe into two columns (variable and value)
     df = pd.melt(df, id_vars=['countyFIPS'])
     return df
 
-
-# Lineplot Visualization Using Streamlit
 def line_plot_vis(df):
+    """
+    Lineplot Visualization Using Streamlit
+    """
     df = lineplot_clean_data(df)
     st.line_chart(df)
     return
 
-# Choropleth Visualization Using Plotly
-# Using plotly express choropleth, plotted per 100,000 cases with an animation that runs through all the weeks
+# 
 def choropleth_confirmed_vis(df):
+    """
+    Choropleth Visualization Using Plotly
+    Using plotly express choropleth, plotted per 100,000 cases with an animation that runs through all the weeks
+    """
     fig = px.choropleth(df, geojson=counties, locations='countyFIPS',
                     color='value',
                     color_continuous_scale="Viridis",
@@ -93,9 +108,11 @@ def choropleth_confirmed_vis(df):
     return fig
 
 
-# Choropleth Visualization Using Plotly
-# Using plotly express choropleth, plotted per 100,000 cases with an animation that runs through all the weeks
 def choropleth_deaths_vis(df):
+    """
+    Choropleth Visualization Using Plotly
+    Using plotly express choropleth, plotted per 100,000 cases with an animation that runs through all the weeks
+    """
     fig = px.choropleth(df, geojson=counties, locations='countyFIPS',
                     color='value',
                     color_continuous_scale="Viridis",
@@ -126,4 +143,4 @@ if __name__ == "__main__":
     choro_deaths = choropleth_clean_data(deaths)
     #choropleth_confirmed_vis(choro_confirm) # Takes a very long time to run
     #choropleth_deaths_vis(choro_deaths) # Takes a very long time to run
-
+    
